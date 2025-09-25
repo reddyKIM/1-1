0c79333a4645cd8c7410f67c90b392efa7cf6830
reddyKIM <khjk1245@gmail.com>
2025-09-26 02:26:36 +0900

    fix: ensure astro build produces dist

---
+++ .eslintrc.cjs
@@ -0,0 +1,47 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    extraFileExtensions: ['.astro'],
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'astro', 'jsx-a11y', 'react', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:astro/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/strict',
+    'prettier'
+  ],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  overrides: [
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro']
+      },
+      rules: {
+        'astro/no-set-html-directive': 'error',
+        'react/react-in-jsx-scope': 'off',
+        'react/no-unknown-property': 'off',
+        'react/jsx-key': 'off'
+      }
+    },
+    {
+      files: ['*.tsx', '*.jsx'],
+      rules: {
+        'react/react-in-jsx-scope': 'off'
+      }
+    }
+  ],
+  ignorePatterns: ['dist', '.astro']
+};
